6cd38db20efbe0206a2e3861de8e27125c2471a5
nadiaxm5 <montana@uji.es>
2025-07-29 14:53:29 +0200

    Tanques completo

--- Assets/PostProcessing/Editor/PropertyDrawers/MinDrawer.cs
+++ Assets/PostProcessing/Editor/PropertyDrawers/MinDrawer.cs
@@ -3,12 +3,12 @@ using UnityEngine.PostProcessing;
 
 namespace UnityEditor.PostProcessing
 {
-    [CustomPropertyDrawer(typeof(MinAttribute))]
-    sealed class MinDrawer : PropertyDrawer
+    [CustomPropertyDrawer(typeof(UnityEngine.MinAttribute))]
+    internal sealed class MinDrawer : PropertyDrawer
     {
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
-            MinAttribute attribute = (MinAttribute)base.attribute;
+            UnityEngine.MinAttribute attribute = (UnityEngine.MinAttribute)base.attribute;
 
             if (property.propertyType == SerializedPropertyType.Integer)
             {
